63c405180ff23340da987a28a893c1e5167ffa2f
Paul Zibarov <paul.zibarov@gmail.com>
2020-04-21 21:55:29 +0300

    Add connection string to configuration file.

---
+++ Langium.DataLayer/LangiumDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Langium.DataLayer
+{
+    public class LangiumDbContext : DbContext
+    {
+        public LangiumDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(DbConnectionProvider.GetConnectionString());
+        }
+    }
+}
