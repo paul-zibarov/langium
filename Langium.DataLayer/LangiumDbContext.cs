0f4c2d7a04f5d65fce0c80059797ecddb83b69d2
Paul Zibarov <paul.zibarov@gmail.com>
2020-04-22 20:27:56 +0300

    Set PK, FK and other constraints to UserModel, StatsModel, UserProfileModel.

--- Langium.DataLayer/LangiumDbContext.cs
+++ Langium.DataLayer/LangiumDbContext.cs
@@ -19,6 +19,82 @@ namespace Langium.DataLayer
             optionsBuilder.UseNpgsql(DbConnectionProvider.GetConnectionString());
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //unique_constraint
+            modelBuilder
+                .Entity<UserModel>()
+                .HasIndex(u => u.Login)
+                .IsUnique();
+
+            modelBuilder
+                .Entity<UserProfileModel>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            //FK_constraint
+            modelBuilder
+                .Entity<UserModel>()
+                .HasOne(u => u.Profile)
+                .WithOne(p => p.User)
+                .HasForeignKey<UserProfileModel>(p => p.UserId);
+
+            modelBuilder
+                .Entity<UserModel>()
+                .HasOne(u => u.Stats)
+                .WithOne(s => s.User)
+                .HasForeignKey<StatsModel>(s => s.UserId);
+
+            //require_constraint
+            modelBuilder
+                .Entity<UserModel>()
+                .Property(u => u.Login)
+                .IsRequired();
+
+            modelBuilder
+                .Entity<UserModel>()
+                .Property(u => u.Password)
+                .IsRequired();
+
+            //size_constraint
+            modelBuilder
+                .Entity<UserProfileModel>()
+                .Property(p => p.Name)
+                .HasMaxLength(30);
+
+            modelBuilder
+                .Entity<UserProfileModel>()
+                .Property(p => p.Email)
+                .HasMaxLength(50);
+
+            modelBuilder
+                .Entity<UserModel>()
+                .Property(u => u.Login)
+                .HasMaxLength(30);
+
+            modelBuilder
+                .Entity<UserModel>()
+                .Property(u => u.Password)
+                .HasMaxLength(30);
+
+            //default_values
+            modelBuilder
+                .Entity<StatsModel>()
+                .Property(s => s.TotalAnswers)
+                .HasDefaultValue(0);
+
+            modelBuilder
+                .Entity<StatsModel>()
+                .Property(s => s.RightAnswers)
+                .HasDefaultValue(0);
+
+            modelBuilder
+                .Entity<UserProfileModel>()
+                .Property(p => p.RegistrationDate)
+                .HasDefaultValueSql("now()")
+                .ValueGeneratedOnAdd();
+        }
+
         public DbSet<UserModel> Users { get; set; }
 
         public DbSet<UserProfileModel> UserProfiles { get; set; }
