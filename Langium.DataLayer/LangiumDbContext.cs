95f04327e5b63c82eee64ea3726d453be0bacdb4
Paul Zibarov <paul.zibarov@gmail.com>
2020-05-22 01:30:29 +0300

    Add Words, Categories, Lexemes, Translations, Transcriptions DbSets and configure methods.

--- Langium.DataLayer/LangiumDbContext.cs
+++ Langium.DataLayer/LangiumDbContext.cs
@@ -25,6 +25,11 @@ namespace Langium.DataLayer
             modelBuilder.Entity<UserModel>(UserConfigure);
             modelBuilder.Entity<ProfileModel>(ProfileConfigure);
             modelBuilder.Entity<StatsModel>(StatsConfigure);
+            modelBuilder.Entity<WordModel>(WordConfigure);
+            modelBuilder.Entity<CategoryModel>(CategoryConfigure);
+            modelBuilder.Entity<LexemeModel>(LexemeConfigure);
+            modelBuilder.Entity<TranscriptionModel>(TranscriptionConfigure);
+            modelBuilder.Entity<TranslationModel>(TranslationConfigure);
         }
 
         private void UserConfigure(EntityTypeBuilder<UserModel> builder)
@@ -48,7 +53,7 @@ namespace Langium.DataLayer
         {
             builder.Property(p => p.Name).HasMaxLength(30); //name size constraint
 
-            builder.HasOne(p => p.Stats).WithOne(s => s.Profile).HasForeignKey<StatsModel>(p => p.UserId); //fk Stats
+            builder.HasOne(p => p.Stats).WithOne(s => s.Profile).HasForeignKey<StatsModel>(p => p.ProfileId); //fk Stats
             builder.HasMany(p => p.Categories).WithOne(s => s.Profile).HasForeignKey(c => c.ProfileId); //fk Categories
 
             builder.Property(p => p.Age).HasDefaultValue(18); //default value age
@@ -60,10 +65,51 @@ namespace Langium.DataLayer
             builder.Property(s => s.RightAnswers).HasDefaultValue(0); //right answers default value
         }
 
+        private void WordConfigure(EntityTypeBuilder<WordModel> builder)
+        {
+            builder.HasOne(w => w.Lexeme).WithOne(l => l.Word).HasForeignKey<LexemeModel>(l => l.WordId); //fk Lexeme
+            builder.HasOne(w => w.Transcription).WithOne(t => t.Word).HasForeignKey<TranscriptionModel>(t => t.WordId); //fk Transcription
+            builder.HasMany(w => w.Translations).WithOne(t => t.Word).HasForeignKey(t => t.WordId); //fk Translations
+        }
+
+        private void CategoryConfigure(EntityTypeBuilder<CategoryModel> builder)
+        {
+            builder.Property(c => c.Name).IsRequired(); //category required constraint
+            builder.Property(c => c.Name).HasMaxLength(30); //category size constraint
+        }
+
+        private void LexemeConfigure(EntityTypeBuilder<LexemeModel> builder)
+        {
+            builder.Property(l => l.Lexeme).IsRequired(); //lexeme required constraint
+            builder.Property(l => l.Lexeme).HasMaxLength(30); //lexeme size constraint
+        }
+
+        private void TranslationConfigure(EntityTypeBuilder<TranslationModel> builder)
+        {
+            builder.Property(t => t.Translation).IsRequired(); //translation required constraint
+            builder.Property(t => t.Translation).HasMaxLength(30); //translation size constraint
+        }
+
+        private void TranscriptionConfigure(EntityTypeBuilder<TranscriptionModel> builder)
+        {
+            builder.Property(t => t.Transcription).IsRequired(); //transcription required constraint
+            builder.Property(t => t.Transcription).HasMaxLength(30); //transcription size constraint
+        }
+
         public DbSet<UserModel> Users { get; set; }
 
         public DbSet<ProfileModel> UserProfiles { get; set; }
 
         public DbSet<StatsModel> Stats { get; set; }
+
+        public DbSet<WordModel> Words { get; set; }
+
+        public DbSet<CategoryModel> Categories { get; set; }
+
+        public DbSet<LexemeModel> Lexemes { get; set; }
+
+        public DbSet<TranscriptionModel> Transcriptions { get; set; }
+
+        public DbSet<TranslationModel> Translations { get; set; }
     }
 }
