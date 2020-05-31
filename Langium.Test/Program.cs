fce9cf6cf940f9b0e0ace26e3f9c069dda40d2dc
Paul Zibarov <paul.zibarov@gmail.com>
2020-05-31 03:16:26 +0300

    Add minor fixes.

--- Langium.Test/Program.cs
+++ Langium.Test/Program.cs
@@ -11,25 +11,6 @@ namespace Langium.Test
     {
         static void Main(string[] args)
         {
-            //var dao = new UserDao();
-
-            //var user = new UserModel();
-            //user.Email = "test@gmail.com";
-            //user.Password = "password";
-            //user.Profile = new ProfileModel()
-            //{
-            //    Name = "Name",
-            //    Stats = new StatsModel(),
-            //    Categories = new List<CategoryModel>()
-            //    {
-            //        new CategoryModel()
-            //        {
-            //            Name = "Default"
-            //        }
-            //    }
-            //};
-
-            //var added = dao.AddUserAsync(user).Result;
         }
     }
 }
