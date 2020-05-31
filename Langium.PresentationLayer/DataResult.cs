7430e9a495c5dbfa6054734029ec1e486931e293
Paul Zibarov <paul.zibarov@gmail.com>
2020-05-31 03:07:38 +0300

    Add base API response.

---
+++ Langium.PresentationLayer/DataResult.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Langium.PresentationLayer
+{
+    public class DataResult<T>
+    {
+        public DataResult (T data)
+        {
+            Succeded = true;
+            Data = data;
+        }
+
+        public DataResult(Exception ex, string errorMessage)
+        {
+            Succeded = false;
+            Exception = ex;
+            ErrorMessage = errorMessage;
+        }
+
+        public T Data { get; set; }
+
+        public bool Succeded { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        [JsonIgnore]
+        public Exception Exception { get; set; }
+    }
+}
