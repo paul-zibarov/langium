8e0b532619441de2620542e0d95184b4e4da1b45
Paul Zibarov <paul.zibarov@gmail.com>
2020-06-01 12:31:43 +0300

    Create Langium.Domain project with Gmail SMTP provider and CheckHelper with email validity check.

---
+++ Langium.Domain/GmailSMTPHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace Langium.Domain
+{
+    public class GmailSMTPHelper
+    {
+        private readonly string _smtpUrl = "smtp.gmail.com";
+
+        private readonly int _port = 587;
+
+        private readonly string _email = "langium.dictionary@gmail.com";
+
+        private readonly string _password = "Langium123";
+
+        public void SendEmail(string receiverEmail, string emailSubject, string emailBody)
+        {
+            if (!string.IsNullOrEmpty(receiverEmail) && !string.IsNullOrEmpty(emailBody))
+            {
+                var client = new SmtpClient(_smtpUrl, _port)
+                {
+                    Credentials = new NetworkCredential(_email, _password),
+                    EnableSsl = true
+                };
+
+                client.Send(_email, receiverEmail, emailSubject, emailBody);
+            }
+        }
+    }
+}
