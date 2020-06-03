343b0c98d75fefcb30d296374c79986dd0280428
Paul Zibarov <paul.zibarov@gmail.com>
2020-06-04 01:52:41 +0300

    Add Category Add and Update Dto's.

---
+++ Langium.PresentationLayer/ViewModels/CategoryUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Langium.PresentationLayer.ViewModels
+{
+    public class CategoryUpdateDto
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
