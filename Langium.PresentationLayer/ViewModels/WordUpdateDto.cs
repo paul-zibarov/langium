db28ef6b971792016007e541531e57cfdbf9e4c4
Paul Zibarov <paul.zibarov@gmail.com>
2020-06-04 21:14:02 +0300

    Add WordAdd and WordUpdate Dto.

---
+++ Langium.PresentationLayer/ViewModels/WordUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Langium.PresentationLayer.ViewModels
+{
+    public class WordUpdateDto
+    {
+        public string Lexeme { get; set; }
+
+        public string Transcription { get; set; }
+
+        public string Translation { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
